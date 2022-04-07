0d12ec10f2b80056af142b7fc8ee1d6aa36671c3
Francisco Vizentin <franciscovizentin@gmail.com>
2022-04-07 15:36:25 +0200

    Added menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,10 +9,17 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
-<body style="background-image: url('./assets/wp.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center; min-height: 100vh">
+<body
+    style="background-image: url('./assets/wp.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center; min-height: 100vh">
 
-<div class="fixed top-4 left-4 w-32">
-    <img src="assets/finalll_big.png" alt="">
+<div class="fixed top-4 left-4 flex h-16 absolute z-10">
+    <img src="assets/finalll_big.png" alt="" class="mr-4">
+    <a href="/buy">
+        <img src="assets/buybutton.png" alt="" class="mr-4">
+    </a>
+    <a href="https://instagram.com/d4t.paradise" target="_blank">
+        <img src="assets/instagrambutton.png" alt="">
+    </a>
 </div>
 
 <script src="/js/app.js"></script>
